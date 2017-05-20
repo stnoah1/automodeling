1334026ac3231ee5d6ca916a59543854d6f35093
stnoah1 <stnoah1@gmail.com>
2017-05-20 11:05:52 +0900

    #1 voxel_to_npz 실행 되게함

---
+++ recognize/converter/voxel_to_npz.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy.io
+
+
+def voxel_to_npz(mat_file_path, npz_file_path, num_rotate=32, res=32):
+    train = scipy.io.loadmat(mat_file_path)
+
+    # Delete extra .matfile stuff
+    del train['__globals__']
+    del train['__header__']
+    del train['__version__']
+
+    targets = np.asarray([], dtype=np.uint8)
+    features = np.zeros((1, 1, res, res, res), dtype=np.uint8)
+
+    # Select which classes to read in
+    class_keys = sorted(train.keys())
+
+    for i, key in enumerate(class_keys):
+        targets = np.append(targets, i * np.ones(num_rotate * len(train[key]), dtype=np.uint8))
+        features = np.append(
+            features,
+            np.reshape(train[key], (num_rotate * np.shape(train[key])[0], 1, res, res, res)),
+            axis=0
+        )
+        if i == 0:
+            features = np.delete(features, 0, axis=0)
+        del train[key]
+    del train
+
+    np.savez_compressed(npz_file_path, **{'features': features, 'targets': targets})
