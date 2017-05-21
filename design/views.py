76bc97cff4d8843a8c56d37e906553ca4d6ef709
Hyun-Tae Hwang <hwanght1@gmail.com>
2017-05-21 14:20:05 +0900

    bridge view 추가

--- design/views.py
+++ design/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
+
+from design.common import get_client_ip
+from design.models import Project
+
+
+def home(request):
+    ip = get_client_ip(request)
+    project = Project.objects.create(ip=ip)
+    return redirect(reverse('design:home', kwargs={'code': project.code}))
 
 
 class DesignView(TemplateView):
