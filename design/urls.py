adfd3d02131f8a7f1761904f87810bc7667bdbc6
Hyun-Tae Hwang <hwanght1@gmail.com>
2017-05-21 14:18:16 +0900

    bridge 추가

--- design/urls.py
+++ design/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DesignView.as_view(), name='home'),
+    url(r'^$', views.home, name='bridge'),
+    url(r'^(?P<code>[^/]+)/$', views.DesignView.as_view(), name='home'),
 ]
