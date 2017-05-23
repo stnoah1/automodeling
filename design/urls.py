d0547f5e6ac360cd33921c79f338fcf2af0563ed
Hyun-Tae Hwang <hwanght1@gmail.com>
2017-05-23 21:25:16 +0900

    converting 을 위한 url  준비

--- design/urls.py
+++ design/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='bridge'),
     url(r'^(?P<code>[^/]+)/$', views.DesignView.as_view(), name='home'),
+    url(r'^(?P<code>[^/]+)/convert/$', views.convert_view, name='convert'),
 ]
