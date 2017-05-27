139b703349c870f2eba0515dc6162f76eac9ec64
stnoah1 <stnoah1@gmail.com>
2017-05-27 18:50:48 +0900

    모델을 최초 한번만 호출하면 되게 initialize와 main 으로 분리함.

--- recognize/classifier/run.py
+++ recognize/classifier/run.py
@@ -15,7 +15,7 @@ from recognize.classifier.utils import checkpoints
 WORKING_DIR = os.getcwd()
 
 
-def make_testing_functions(cfg, model):
+def classifing_function(cfg, model):
     # Input Array
     X = T.TensorType('float32', [False] * 5)('X')
 
@@ -32,13 +32,11 @@ def make_testing_functions(cfg, model):
     # Get output
     y_hat_deterministic = lasagne.layers.get_output(l_out, X, deterministic=True)
 
-    # Average across rotation examples
-    # pred = T.argmax(T.sum(y_hat_deterministic, axis=0))
-
     # Compile Functions
     fc_vector = theano.function([batch_index], [T.sum(y_hat_deterministic, axis=0)], givens={
         X: X_shared[test_batch_slice]
     })
+
     tfuncs = {'fc_vector': fc_vector}
     tvars = {'X': X,
              'X_shared': X_shared,
@@ -51,7 +49,7 @@ class WrongModelError(object):
     pass
 
 
-def main(data_path=os.path.join(WORKING_DIR, 'converter', 'tmp', 'voxel.npz'), model='VRN'):
+def initialize(model='VRN'):
     if model == 'VRN':
         from recognize.classifier.models import VRN as config_module
     else:
@@ -67,16 +65,17 @@ def main(data_path=os.path.join(WORKING_DIR, 'converter', 'tmp', 'voxel.npz'), m
 
     # Compile functions
     print('Compiling theano functions...!!')
-    tfuncs, tvars, model = make_testing_functions(cfg, model)
+    tfuncs, tvars, model = classifing_function(cfg, model)
 
     # Load weights
     checkpoints.load_weights(weights_fname, model['l_out'])
 
+    return tfuncs, tvars
+
+
+def main(tvars, tfuncs, data_path):
     xt = np.asarray(np.load(data_path)['features'], dtype=np.float32)
-
-    n_rotations = cfg['n_rotations']
-
-    x_shared = np.asarray(xt[0:n_rotations, :, :, :, :], dtype=np.float32)
+    x_shared = np.asarray(xt[0:12, :, :, :, :], dtype=np.float32)
 
     # Prepare data
     tvars['X_shared'].set_value(4.0 * x_shared - 1.0, borrow=True)
@@ -85,4 +84,5 @@ def main(data_path=os.path.join(WORKING_DIR, 'converter', 'tmp', 'voxel.npz'), m
 
 
 if __name__ == '__main__':
-    main('../converter/tmp/voxel.npz')
+    tfuncs, tvars = initialize(model='VRN')
+    main(tfuncs, tvars, '../converter/tmp/voxel.npz')
